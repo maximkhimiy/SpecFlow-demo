80bc85e45f6ed01e373efc11e1664883a32c2eea
mkhimiy-x <khimiy.maxim.tunein@gmail.com>
2016-02-15 19:32:01 +0200

    the whole project

---
+++ SpecFlowDemo/Features/LogIn_Feature.feature
@@ -0,0 +1,15 @@
+﻿Feature: LogIn_Feature
+	In order to access my account
+    As a user of the website
+    I want to log into the website
+ 
+@mytag
+Scenario Outline: Successful Login with Valid Credentials
+	Given User is at the Login Page
+	When User enter <username> and <password>
+	And Click on the LogIn button
+	Then Successful LogIN message should display
+	Examples:
+	| username | password |
+	| subscriber | subscriberpassword |
+	| fake_name | fake_pass |
---
+++ SpecFlowDemo/LogIn_FeatureSteps.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowDemo
+{
+    [Binding]
+    public class LogIn_FeatureSteps
+    {
+        public IWebDriver driver;
+
+        [Given(@"User is at the Login Page")]
+        public void GivenUserIsAtTheHomePage()
+        {
+            driver = new FirefoxDriver();
+            driver.Url = "http://autoqa.pp.ua/wp-admin";
+        }
+
+        [When(@"User enter UserName and Password")]
+        public void WhenUserEnterUserNameAndPassword()
+        {
+            driver.FindElement(By.Id("user_login")).SendKeys("subscriber");
+            driver.FindElement(By.Id("user_pass")).SendKeys("subscriberpassword");
+        }
+
+        [When(@"Click on the LogIn button")]
+        public void WhenClickOnTheLogInButton()
+        {
+            driver.FindElement(By.Id("wp-submit")).Click();
+        }
+
+        [When(@"User enter (.*) and (.*)")]
+        public void WhenUserEnterAnd(string username, string password)
+        {
+            driver.FindElement(By.Id("user_login")).SendKeys(username);
+            driver.FindElement(By.Id("user_pass")).SendKeys(password);
+        }
+
+        [Then(@"Successful LogIN message should display")]
+        public void ThenSuccessfulLogINMessageShouldDisplay()
+        {
+            if (driver.FindElement(By.Id("profile-page")).Displayed)
+            {
+                // Print a Log In message to the screen 
+                Console.WriteLine("Login successfully.");
+            }
+
+            // Close the driver
+
+            driver.Close();
+        }
+
+
+    }
+}
